58391a2bc5e69d43b93a85ed0d05ed1f78cea160
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-04-28 19:39:07 +0300

    Create task1.py
    Create task1.py

---
+++ task1.py
@@ -0,0 +1,32 @@
+from queue import Queue
+import uuid
+
+#Створити чергу заявок
+q = Queue()
+
+
+#Функція generate_request
+def generate_request():
+    print("Generating request ...")
+    request = uuid.uuid4()
+    q.put(request)
+    print(f"Request {request} added to queqe")
+
+
+#Функція process_request():
+def process_request():
+    print("Processing request ...")
+    if not q.empty(): 
+        request = q.get()
+        print(f"Processing request {request}")
+    else:
+        print(f"The is no active requests")
+
+
+while True:
+    generate_request()
+    process_request()
+
+        
+
+
